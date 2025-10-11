dab48d1830ebebb1d2621ff00181d3ee835bcec4
Syed Kaleem Shah <157974883+KaleemShahG@users.noreply.github.com>
2025-10-11 10:34:57 +0000

    learn operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,13 +1,63 @@
-let score = false
+// let score = false
  
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 
-let valueInNumber = Number (score)
-console.log(valueInNumber);
-/*
-"33" => 33
-"33abc" => NaN
-true => 1 ; false => 0 */
+// let valueInNumber = Number (score)
+// console.log(valueInNumber);
+// /*
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0 */
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+
+// // 1=> true ;
+// // 0 => false
+// // "kaleem" => true
+
+// let AccountNo = 1234
+
+// let NumbertoBoolean = Object (AccountNo)
+
+// // console.log(AccountNo);
+// console.log(typeof AccountNo );
+// console.log(typeof NumbertoBoolean);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 =" hello ";
+// let str2 = " kaleem"
+// let str3 = str1+str2
+// console.log(str1 +str2);
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(2+"1");
+
+// let gameCounter =100
+// gameCounter++ ;
+// console.log(gameCounter);
+
+let x =3;
+let y= x++;
+
+
+console.log(y , x);
+
+
+
+
+
+
 
