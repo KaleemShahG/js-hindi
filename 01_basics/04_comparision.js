df32e4575d55fe95968d1ee3ce05450fa4754b3e
Syed Kaleem Shah <157974883+KaleemShahG@users.noreply.github.com>
2025-10-11 12:04:48 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,17 @@
+// console.log(2<1); //false
+// console.log(1<2); //true
+// console.log(4==4); //true
+// console.log(1 != 1); //false
+
+// console.log("1">2);
+// console.log("02">1);
+
+console.log(null >0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+
+
+
+
