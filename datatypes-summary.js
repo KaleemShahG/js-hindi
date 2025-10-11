a312ec5d8c70bf0e1f39800f33e852c174e092ea
Syed Kaleem Shah <157974883+KaleemShahG@users.noreply.github.com>
2025-10-11 13:44:50 +0000

    learning data types

---
+++ datatypes-summary.js
@@ -0,0 +1,35 @@
+/*primitive
+
+7 types : String , Number , Boolean , null , undefined,Symbol,BigInt
+
+Reference || non primitive
+
+array , Objects, Functions
+*/
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false 
+const outsideTemp=null
+ let userEmail ;
+
+ console.log(userEmail);
+
+ const id = Symbol('123')
+ const anotherId = Symbol('123')
+
+ console.log(id === anotherId);
+ 
+
+ const heros = ["shaktiman", "naaraj","doga"];
+ let myObj={
+    name :"hitesh",
+    age :"22",
+ }
+
+ const myFunction = function(){
+    console.log("hello world");  
+ }
+
+ console.log(myFunction);
