79e68e873c7ed339e377b9d10231194796f94a11
Syed Kaleem Shah <157974883+KaleemShahG@users.noreply.github.com>
2025-10-14 17:12:28 +0000

    learn string

---
+++ 05_strings.js
@@ -0,0 +1,16 @@
+const name = " kaleem ";
+const repoCount =  50 ;
+
+console.log(name  + repoCount  + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String ('kaleem')
+console.log(gameName.__proto__);
+
+console.log(gameName.indexOf('k'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+
